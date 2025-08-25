d9b68c7ae98df21ae6505358cb4e4c9ff28a792a
Ivan A Rodriguez <ivancalvo91@gmail.com>
2025-08-25 13:29:17 -0400

    configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-	return <></>;
+	return (
+		<div className='flex min-h-screen items-center justify-center'>
+			<h1 className='text-7xl'>Hello World!</h1>
+		</div>
+	);
 }
 
 export default App;
