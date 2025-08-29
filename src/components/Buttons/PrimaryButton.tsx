176f0dbd0c8f598f3e443cbe9681df118bb15ccd
Ivan A Rodriguez <ivancalvo91@gmail.com>
2025-08-29 09:18:59 -0400

    Enhance Primary button and avoid adding styles from outside

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,21 +1,15 @@
 import type { BaseButtonProps } from './BaseButton';
 import BaseButton from './BaseButton';
 
-type PrimaryButtonProps = Omit<BaseButtonProps, 'className'> & {
-	className?: string;
-};
+type PrimaryButtonProps = Omit<BaseButtonProps, 'className' | 'style'> & {};
 
-export default function PrimaryButton({
-	className = '',
-	...props
-}: PrimaryButtonProps) {
+export default function PrimaryButton({ ...props }: PrimaryButtonProps) {
 	return (
 		<BaseButton
-			className={`
+			className='
         relative overflow-hidden rounded-full
         bg-pink-500 text-white hover:bg-pink-600
-        ${className}
-      `}
+      '
 			{...props}
 		/>
 	);
